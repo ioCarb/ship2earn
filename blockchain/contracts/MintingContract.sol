f9749104b1e42c3bc8af90dc66f9da1522d05abf
Schille00 <84453633+Schille00@users.noreply.github.com>
2024-05-29 20:08:08 +0200

    Add files via upload
    ##MintingContract: 
    - creates ERC20 instance (CarbToken)
    - offers 3 certificates for specific prices
    - handles minting/burning via Role-based Access Control
    
    ##CertificateContract:
    - Creates ERC721 instance (Carbon Certificate)
    - Handles Certificates (look-up, mint, burn (after time wip))
    - Makes the Minting Contract sole Minter (Role-based Access Control)

---
+++ blockchain/contracts/MintingContract.sol
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+//import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract CarbToken is ERC20, AccessControl	{
+
+    address certificateAddress;
+
+    //0xa49807205ce4d355092ef5a8a18f56e8913cf4a201fbe287825b095693c21775
+    bytes32 private constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
+
+    //0x9f2df0fed2c77648de5860a4cc508cd0818c85b8b8a1ab4ceeef8d981c8956a6
+    bytes32 private constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+    //0x3c11d16cbaffd01df69ce1c404f6340ee057498f5f00246190ea54220576a848
+    //bytes32 private constant BURNER_ROLE = keccak256("BURNER_ROLE");
+
+    constructor() ERC20("Carb_Token", "CRB") {
+        // Grant the default admin role to a specified iocarb account
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(MINTER_ROLE, msg.sender);
+    }
+
+    function mint(uint256 _amount) public onlyRole(MINTER_ROLE) {
+        _mint(msg.sender, _amount);
+    }
+
+    function burn(address _from, uint256 _amount) public {
+        require(msg.sender == _from, "Caller is not owner");
+        _burn(_from, _amount);
+    }
+
+    function buyGreenCertificate() external {
+        require(balanceOf(msg.sender) >= 300, "Insufficient balance");
+        burn(msg.sender, 300);
+        ICertificateContract(certificateAddress).safeMintGreen(msg.sender);
+    }
+
+    function buyYellowCertificate() external {
+        require(balanceOf(msg.sender) >= 200, "Insufficient balance");
+        burn(msg.sender, 200);
+        ICertificateContract(certificateAddress).safeMintYellow(msg.sender);
+    }
+
+    function buyRedCertificate() external {
+        require(balanceOf(msg.sender) >= 100, "Insufficient balance");
+        burn(msg.sender, 100);
+        ICertificateContract(certificateAddress).safeMintRed(msg.sender);
+    }
+
+    function setCertificateAddress(address _certificateAddress) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        certificateAddress = _certificateAddress;
+    }
+
+}
+
+interface ICertificateContract {
+    function safeMintGreen(address _to) external;
+    function safeMintYellow(address _to) external;
+    function safeMintRed(address _to) external;
+}
