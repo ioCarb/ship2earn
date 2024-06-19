b4f9b581ad5435a1049e44383e0a80ddaac9f720
Felix Dumbeck <felix@dumbeck.net>
2024-06-19 04:23:28 +0200

    compute proving_key on webstream

--- webstream-applet/src/lib.rs
+++ webstream-applet/src/lib.rs
@@ -3,21 +3,27 @@ use rand::rngs::StdRng;
 use rand::SeedableRng;
 use rust_embed::Embed;
 use std::io::{Cursor, Read, Write};
+use typed_arena::Arena;
 use ws_sdk::database::kv::{get, set};
 use ws_sdk::log::log_error;
 use zokrates_ark::Ark;
-use zokrates_ast::ir::{self, ProgEnum};
+use zokrates_ast::ir::{self, ProgEnum, Witness};
 use zokrates_ast::typed::types::{ConcreteSignature, ConcreteType, GTupleType};
+use zokrates_core::compile::{compile, CompileError};
 use zokrates_field::Field;
 use zokrates_proof_systems::rng::get_rng_from_entropy;
-use zokrates_proof_systems::{Backend, Scheme, G16};
+use zokrates_proof_systems::{Backend, NonUniversalBackend, Scheme, G16};
 
 #[cfg(not(debug_assertions))]
 use ws_sdk::log::log_info;
 
 #[derive(Embed)]
-#[folder = "./inputs/"]
+#[folder = "./inputs-test"]
 struct Inputs;
+
+// #[derive(Embed)]
+// #[folder = "../../ZoKrates/zokrates_stdlib/stdlib/"]
+// struct Stdlib;
 
 #[cfg(debug_assertions)]
 fn log_info(str: &str) -> Result<()> {
@@ -67,20 +73,40 @@ pub extern "C" fn start(_: i32) -> i32 {
 
 pub fn witness(_: i32) -> i32 {
     log_info("from witness").unwrap();
+
     // read compiled program
-
     let out = Inputs::get("out").unwrap();
-    let root = Inputs::get("root.zok").unwrap();
 
-    log_info(std::str::from_utf8(root.data.as_ref()).unwrap()).unwrap();
+    // log_info(std::str::from_utf8(root.data.as_ref()).unwrap()).unwrap();
 
-    let mut reader = Cursor::new(out.data.as_ref());
+    let mut out_reader = Cursor::new(out.data.as_ref());
 
     log_info("1").unwrap();
-    // might want to use bellman?
-    match ProgEnum::deserialize(&mut reader).unwrap() {
+
+    // might want to use bellman instead of ark?
+    let witness = match ProgEnum::deserialize(&mut out_reader).unwrap() {
         ProgEnum::Bn128Program(p) => compute_witness::<_, _, G16, Ark>(p),
         _ => panic!(),
+    }
+    .unwrap();
+
+    log_info("computed witness").unwrap();
+
+    let mut out_reader = Cursor::new(out.data.as_ref());
+
+    let pk_reader = match ProgEnum::deserialize(&mut out_reader).unwrap() {
+        ProgEnum::Bn128Program(p) => compute_proving_key::<_, _, G16, Ark>(p),
+        _ => panic!("asdf"),
+    }
+    .unwrap();
+
+    log_info("computed pk").unwrap();
+
+    let mut out_reader = Cursor::new(out.data.as_ref());
+
+    match ProgEnum::deserialize(&mut out_reader).unwrap() {
+        ProgEnum::Bn128Program(p) => compute_proof::<_, _, G16, Ark>(p, pk_reader, witness),
+        _ => panic!("abcdef"),
     }
     .unwrap();
 
@@ -88,15 +114,35 @@ pub fn witness(_: i32) -> i32 {
     0
 }
 
+// fn ws_compile<T: Field>() -> Result<(), String> {
+//     let path = std::path::Path::from("root.zok");
+//     let stdlib_path = std::path::Path::from("root.zok");
+//     let root = Inputs::get("root.zok").unwrap();
+//     let source = String::from_utf8(root.data.into_owned()).unwrap();
+//     let arena = Arena::new();
+//     let resolver = zokrates_fs_resolver::FileSystemResolver::with_stdlib_root(stdlib_path);
+
+//     let artifacts = compile::<T, _>(source, path.clone(), Some(&resolver), config, &arena)
+//         .map_err(|e| {
+//             format!(
+//                 "Compilation failed:\n\n{}",
+//                 e.0.iter().map(fmt_error).collect::<Vec<_>>().join("\n\n")
+//             )
+//         })
+//         .unwrap();
+
+//     Ok(())
+// }
+
 fn compute_witness<
     'a,
     T: Field,
     I: Iterator<Item = ir::Statement<'a, T>>,
-    S: Scheme<T>,
+    S: Scheme<T> + zokrates_proof_systems::NonUniversalScheme<T>,
     B: Backend<T, S>,
 >(
     mut ir_prog: ir::ProgIterator<'a, T, I>,
-) -> Result<(), String> {
+) -> Result<Witness<T>, String> {
     log_info("Computing witness...").unwrap();
 
     let signature = ConcreteSignature::new()
@@ -107,6 +153,7 @@ fn compute_witness<
 
     // get arguments
     let raw_arguments = "337 113569";
+    log_info(&format!("inputs: {}", raw_arguments)).unwrap();
     let arguments = raw_arguments
         .split(' ')
         .map(|x| T::try_from_dec_str(x))
@@ -138,13 +185,42 @@ fn compute_witness<
     log_info(&format!("witness: {}", test)).unwrap();
     log_info(&format!("size: {}", writer.len().to_string())).unwrap();
 
-    // let witness_reader = BufReader::new(writer);
+    Ok(witness)
+}
 
-    // let witness = ir::Witness::read(witness_reader)
-    //     .map_err(|why| format!("Could not load witness: {:?}", why))?;
+fn compute_proving_key<
+    'a,
+    T: Field,
+    I: Iterator<Item = ir::Statement<'a, T>>,
+    S: Scheme<T> + zokrates_proof_systems::NonUniversalScheme<T>,
+    B: NonUniversalBackend<T, S>,
+>(
+    mut ir_prog: ir::ProgIterator<'a, T, I>,
+) -> Result<Cursor<Vec<u8>>, String> {
+    // TODO: share entropy maybe
+    let mut rng = StdRng::from_entropy();
 
-    let proving_key = Inputs::get("proving.key").unwrap();
-    let pk_reader = Cursor::new(proving_key.data.as_ref());
+    let keypair = B::setup(ir_prog, &mut rng);
+    let mut pk_reader = Cursor::new(keypair.pk.to_owned());
+    
+    log_info("in here");
+    // let proving_key = Inputs::get("proving.key").unwrap();
+    // let pk_reader = Cursor::new(proving_key.data.into_owned());
+
+    Ok(pk_reader)
+}
+
+fn compute_proof<
+    'a,
+    T: Field,
+    I: Iterator<Item = ir::Statement<'a, T>>,
+    S: Scheme<T>,
+    B: Backend<T, S>,
+>(
+    mut ir_prog: ir::ProgIterator<'a, T, I>,
+    pk_reader: impl Read,
+    witness: Witness<T>,
+) -> Result<(), String> {
     let mut rng = StdRng::from_entropy();
 
     let proof = B::generate_proof(ir_prog, witness, pk_reader, &mut rng);
@@ -155,32 +231,6 @@ fn compute_witness<
     .unwrap();
 
     log_info(proof.as_str()).unwrap();
-
-    //print!("{:?}", writer);
-
-    // print!("{:?}", writer);
-    // let output = match get("witness") {
-    //     Ok(str) => str,
-    //     Err(_) => {
-    //         log_error("couldnt find witness");
-    //         panic!()
-    //     }
-    // };
-
-    // set("asdf", "asdf".into());
-    // let test = get("asdf").unwrap();
-    // log_info(std::str::from_utf8(&test).unwrap()).unwrap();
-
-    // let mut output = &mut Default::default();
-    // writer.read_to_string(output).unwrap();
-    // print!("{:?}", output);
-    // log_info(&format!("size: {}", output.len())).unwrap();
-
-    // set("witness", output.to_vec());
-
-    // log_info(output).unwrap();
-
-    // log_info("finished computing witness").unwrap();
 
     Ok(())
 }
