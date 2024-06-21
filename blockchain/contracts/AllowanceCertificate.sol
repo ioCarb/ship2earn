f207d4df376202070ec00f731fc4666b946dbe72
ykpyck <kopyciok@campus.tu-berlin.de>
2024-06-21 10:04:43 +0200

    sync

--- blockchain/contracts/AllowanceCertificate.sol
+++ blockchain/contracts/AllowanceCertificate.sol
@@ -2,14 +2,55 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract AllowanceCertificate is ERC721, ERC721URIStorage, AccessControl {
-    bytes32 private constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
+/**
+ * @title AllowanceCertificate
+ * @dev ERC721 token contract for carbon certificates
+ * TODO: 
+ * TODO: 
+ */
 
-    constructor() ERC721("AllowanceCertificate", "AC") {
+contract AllowanceCertificate is ERC721, AccessControl {
+    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
+
+    // Mapping from token ID to IPFS CID
+    mapping(uint256 => string) private _tokenCIDs;
+
+    // Base URI for constructing token URIs
+    string private _baseTokenURI;
+
+    constructor(string memory baseTokenURI) ERC721("MyIPFSNFT", "MIPFS") {
+        _baseTokenURI = baseTokenURI;
         _grantRole(ADMIN_ROLE, msg.sender);
     }
 
+    // Function to mint a new token and set its CID
+    function mint(address to, uint256 tokenId, string memory cid) public onlyRole(ADMIN_ROLE) {
+        _mint(to, tokenId);
+        _setTokenCID(tokenId, cid);
+    }
+
+    // Internal function to set the CID for a token
+    function _setTokenCID(uint256 tokenId, string memory cid) internal virtual {
+        require(_ownerOf(tokenId) != address(0), "ERC721Metadata: CID set of nonexistent token");
+        _tokenCIDs[tokenId] = cid;
+    }
+
+    // Override tokenURI to return the full IPFS link
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        require(_ownerOf(tokenId) != address(0), "ERC721Metadata: URI query for nonexistent token");
+
+        string memory cid = _tokenCIDs[tokenId];
+        return string(abi.encodePacked(_baseTokenURI, cid));
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721, AccessControl) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+
+    // Public function to set a new base URI
+    function setBaseURI(string memory baseTokenURI) public onlyRole(ADMIN_ROLE) {
+        _baseTokenURI = baseTokenURI;
+    }
 }
--- blockchain/contracts/CarbToken.sol
+++ blockchain/contracts/CarbToken.sol
@@ -5,6 +5,13 @@ pragma solidity ^0.8.24;
 import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+/**
+ * @title CarbToken
+ * @dev ERC20 token contract for carbon credits
+ * TODO: 
+ * TODO: 
+ */
+
 contract CarbToken is ERC20, AccessControl	{
     bytes32 private constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
